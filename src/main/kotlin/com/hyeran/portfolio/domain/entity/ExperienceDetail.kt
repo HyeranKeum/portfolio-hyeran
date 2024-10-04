7f0aa62fd536b0e821f46b87cec8a975cd6638a2
HyeranKeum <sarah2316@g.skku.edu>
2024-10-04 19:59:27 +0900

    엔티티 개발 - 연관관계 있음

--- src/main/kotlin/com/hyeran/portfolio/domain/entity/ExperienceDetail.kt
+++ src/main/kotlin/com/hyeran/portfolio/domain/entity/ExperienceDetail.kt
@@ -4,11 +4,25 @@ import jakarta.persistence.*
 
 
 @Entity
-class ExperienceDetail : BaseEntity() {
+class ExperienceDetail(
+    content: String,
+    isActive: Boolean
+) : BaseEntity() {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "experience_detail_id")
     var id: Long? = null
 
+    var content: String = content
+
+    var isActive: Boolean = isActive
+
+    fun update(
+        content: String,
+        isActive: Boolean
+    ) {
+        this.content = content
+        this.isActive = isActive // 역으로 Experience 접근x -> 일대다 단방향 연관관계
+    }
 }
--- src/main/kotlin/com/hyeran/portfolio/domain/entity/Project.kt
+++ src/main/kotlin/com/hyeran/portfolio/domain/entity/Project.kt
@@ -4,11 +4,73 @@ import jakarta.persistence.*
 
 
 @Entity
-class Project : BaseEntity() {
+class Project(
+    name: String,
+    description: String,
+    startYear: Int,
+    startMonth: Int,
+    endYear: Int?,
+    endMonth: Int?,
+    isActive: Boolean
+) : BaseEntity() {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "project_id")
     var id: Long? = null
 
+    var name: String = name
+
+    var description: String = description
+
+    var startYear: Int = startYear
+
+    var startMonth: Int = startMonth
+
+    var endYear: Int? = endYear
+
+    var endMonth: Int? = endMonth
+
+    var isActive: Boolean = isActive
+
+    @OneToMany(
+        targetEntity = ProjectDetail::class, // 아래와 일대다의 관계임을 알려주는 것
+        fetch = FetchType.LAZY,
+        cascade = [CascadeType.ALL]
+    ) //
+    @JoinColumn(name = "project_id") // 맵핑 기준
+    var details: MutableList<ProjectDetail> = mutableListOf()
+
+    @OneToMany(mappedBy = "project") // 양방향 연관관계에서 연관관계 주인 지정
+    var skills: MutableList<ProjectSkill> = mutableListOf()
+
+    fun getEndYearMonth(): String {
+        if (endYear == null || endMonth == null) {
+            return "Present"
+        }
+        return "${endYear}.${endMonth}"
+    }
+
+    fun update(
+        name: String,
+        description: String,
+        startYear: Int,
+        startMonth: Int,
+        endYear: Int?,
+        endMonth: Int?,
+        isActive: Boolean
+    ) {
+        this.name = name
+        this.description = description
+        this.startYear = startYear
+        this.endYear = endYear
+        this.endMonth = endMonth
+        this.isActive = isActive
+    }
+
+    fun addDetails(details: MutableList<ProjectDetail>?) {
+        if (details != null) {
+            this.details.addAll(details)
+        }
+    }
 }
