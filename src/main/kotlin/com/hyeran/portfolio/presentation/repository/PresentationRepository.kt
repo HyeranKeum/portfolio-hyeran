2ed56472f2c9862a12e80e1412e4bcba1a1c9989
HyeranKeum <sarah2316@g.skku.edu>
2024-10-09 13:58:06 +0900

    서비스 개발

--- src/main/kotlin/com/hyeran/portfolio/presentation/repository/PresentationRepository.kt
+++ src/main/kotlin/com/hyeran/portfolio/presentation/repository/PresentationRepository.kt
@@ -1,6 +1,39 @@
 package com.hyeran.portfolio.presentation.repository
 
+import com.hyeran.portfolio.domain.entity.*
+import com.hyeran.portfolio.domain.repository.*
 import org.springframework.stereotype.Repository
 
 @Repository
-class PresentationRepository
+class PresentationRepository(
+    private val achievementRepository: AchievementRepository,
+    private val experienceRepository: ExperienceRepository,
+    private val introductionRepository: IntroductionRepository,
+    private val linkRepository: LinkRepository,
+    private val projectRepository: ProjectRepository,
+    private val skillRepository: SkillRepository,
+) {
+    fun getActiveAchievements(): List<Achievement> {
+        return achievementRepository.findAllByIsActive(true)
+    }
+
+    fun getActiveExperience(): List<Experience> {
+        return experienceRepository.findAllByIsActive(true)
+    }
+
+    fun getActiveIntroduction(): List<Introduction> {
+        return introductionRepository.findAllByIsActive(true)
+    }
+
+    fun getActiveLink(): List<Link> {
+        return linkRepository.findAllByIsActive(true)
+    }
+
+    fun getActiveProject(): List<Project> {
+        return projectRepository.findAllByIsActive(true)
+    }
+
+    fun getActiveSkill(): List<Skill> {
+        return skillRepository.findAllByIsActive(true)
+    }
+}
