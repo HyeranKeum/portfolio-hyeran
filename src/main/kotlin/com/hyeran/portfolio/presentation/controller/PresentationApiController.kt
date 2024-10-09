4b1030db54ff5b6f69cf4fbcbd6c9d13636b60f8
HyeranKeum <sarah2316@g.skku.edu>
2024-10-09 20:01:31 +0900

    컨트롤러 개발

--- src/main/kotlin/com/hyeran/portfolio/presentation/controller/PresentationApiController.kt
+++ src/main/kotlin/com/hyeran/portfolio/presentation/controller/PresentationApiController.kt
@@ -1,15 +1,41 @@
 package com.hyeran.portfolio.presentation.controller
 
+import com.hyeran.portfolio.presentation.dto.IntroductionDTO
+import com.hyeran.portfolio.presentation.dto.LinkDTO
+import com.hyeran.portfolio.presentation.dto.ProjectDTO
+import com.hyeran.portfolio.presentation.dto.ResumeDTO
+import com.hyeran.portfolio.presentation.service.PresentationService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api") // 하위에 있는 애들한테 전부 /api 경로 앞에 추가해줘
-class PresentationApiController {
+class PresentationApiController(
+    private val presentationService: PresentationService
+) {
 
     @GetMapping("/test")
     fun test(): String {
         return "OK" // restcontroller가 그대로 body에 넣어 출력
     }
+
+    @GetMapping("/v1/introductions")
+    fun getIntroductions(): List<IntroductionDTO>{
+        return presentationService.getIntroductions()
+    }
+
+    @GetMapping("/v1/links")
+    fun getLink(): List<LinkDTO>{
+        return presentationService.getLinks()
+    }
+    @GetMapping("/v1/resume")
+    fun getResume(): ResumeDTO{
+        return presentationService.getResume()
+    }
+    @GetMapping("/v1/projects")
+    fun getProjects(): List<ProjectDTO>{
+        return presentationService.getProjects()
+    }
+
 }
--- src/main/kotlin/com/hyeran/portfolio/presentation/controller/PresentationViewController.kt
+++ src/main/kotlin/com/hyeran/portfolio/presentation/controller/PresentationViewController.kt
@@ -1,13 +1,48 @@
 package com.hyeran.portfolio.presentation.controller
 
+import ch.qos.logback.core.model.Model
+import com.hyeran.portfolio.domain.constant.SkillType
+import com.hyeran.portfolio.presentation.service.PresentationService
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller // controller는 기본적으로 html울 반환한다. (controller, service, repository)
-class PresentationViewController {
+class PresentationViewController(
+    private val presentationService: PresentationService
+) {
 
     @GetMapping("/test") // http 메서드를 지정 -> 여기서는 get
     fun test(): String {
         return "test"
+    }
+
+    @GetMapping("/")
+    fun index(model: org.springframework.ui.Model): String{
+        val introductions = presentationService.getIntroductions()
+        model.addAttribute("introductions", introductions)
+
+        val links = presentationService.getLinks()
+        model.addAttribute("links", links)
+
+        return "presentation/index"
+    }
+
+    @GetMapping("/resume")
+    fun resume(model: org.springframework.ui.Model): String {
+
+        val resume = presentationService.getResume()
+        model.addAttribute("resume", resume)
+        model.addAttribute("skillTypes", SkillType.values())
+
+        return "presentation/resume"
+    }
+
+    @GetMapping("/projects")
+    fun projects(model: org.springframework.ui.Model): String {
+
+        val projects = presentationService.getProjects()
+        model.addAttribute("projects", projects)
+
+        return "presentation/projects"
     }
 }
