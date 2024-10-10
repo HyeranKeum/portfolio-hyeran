bd072674ad32f8ba5d52eee22a0fd30bab0e8fb0
HyeranKeum <sarah2316@g.skku.edu>
2024-10-10 18:39:33 +0900

    인터셉터 개발

--- src/main/kotlin/com/hyeran/portfolio/presentation/intercepter/PresentationInterceptor.kt
+++ src/main/kotlin/com/hyeran/portfolio/presentation/intercepter/PresentationInterceptor.kt
@@ -1,6 +1,24 @@
 package com.hyeran.portfolio.presentation.intercepter
 
+import com.hyeran.portfolio.domain.entity.HttpInterface
+import com.hyeran.portfolio.domain.repository.HttpInterfaceRepository
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
 import org.springframework.stereotype.Component
+import org.springframework.web.servlet.HandlerInterceptor
 
 @Component
-class PresentationInterceptor
+class PresentationInterceptor(
+    private val httpInterfaceRepository: HttpInterfaceRepository
+) : HandlerInterceptor {
+
+    override fun afterCompletion(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        handler: Any,
+        ex: Exception?
+    ) {
+        val httpInterface = HttpInterface(request)
+        httpInterfaceRepository.save(httpInterface)
+    }
+}
