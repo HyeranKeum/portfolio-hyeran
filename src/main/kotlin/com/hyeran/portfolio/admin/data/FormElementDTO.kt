01b647c625a1e3a8b76d71ef2412a90892cd2d34
HyeranKeum <sarah2316@g.skku.edu>
2024-10-11 10:35:49 +0900

    공통 개발 - DTO

--- src/main/kotlin/com/hyeran/portfolio/admin/data/FormElementDTO.kt
+++ src/main/kotlin/com/hyeran/portfolio/admin/data/FormElementDTO.kt
@@ -5,3 +5,25 @@ abstract class FormElementDTO(
     val size: Int,
     val type: String
 )
+
+class TextFormElementDTO(
+    name: String,
+    size: Int
+) : FormElementDTO(name = name, size = size, type = "text")
+
+
+class DataFormElementDTO(
+    name: String,
+    size: Int
+) : FormElementDTO(name = name, size = size, type = "date")
+
+
+class SelectFormElementDTO(
+    name: String,
+    size: Int,
+    options: List<Any>
+) : FormElementDTO(name = name, size = size, type = "select") {
+    val options: List<Any> = options
+}
+
+
--- src/main/kotlin/com/hyeran/portfolio/admin/data/TableDTO.kt
+++ src/main/kotlin/com/hyeran/portfolio/admin/data/TableDTO.kt
@@ -1,7 +1,42 @@
 package com.hyeran.portfolio.admin.data
 
+import kotlin.reflect.KClass
+import kotlin.reflect.full.memberProperties
+
 class TableDTO(
     val name: String,
     val columns: List<String>,
     val records: List<List<String>>
-)
+) {
+    companion object {
+        fun <T : Any> from(classInfo: KClass<T>, entities: List<Any>, vararg filterings: String): TableDTO {
+            val name = classInfo.simpleName ?: "Unknown"
+            val columns = createColumns(classInfo, *filterings)
+            //2중 포문, 엔티티마다 돈다. 컬럼명이 동일하면 그 컬럼을 취합한다. 그 취합한 결과물을 반환
+            val records = entities.map { entity ->
+                columns.map { column ->
+                    classInfo.memberProperties
+                        .find { column.equals(it.name) }
+                        ?.getter
+                        ?.call(entity)
+                        .toString()
+                }.toList()
+            }.toList()
+
+            return TableDTO(name = name, columns = columns, records = records)
+        }
+
+        private fun <T : Any> createColumns(classInfo: KClass<T>, vararg filterings: String): MutableList<String> {
+            val mainColumns = classInfo.java.declaredFields
+                .filter { !filterings.contains(it.name) }
+                .map { it.name }
+                .toMutableList()
+
+            val baseColumns = classInfo.java.superclass.declaredFields
+                .map { it.name }
+                .toMutableList()
+
+            return (mainColumns + baseColumns).toMutableList()
+        }
+    }
+}
